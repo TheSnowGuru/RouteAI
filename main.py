151dc06c608d573441f33965422e5dfa198754ac
Shay Ronen <shayronen@gmail.com>
2024-06-04 13:54:56 +0300

    update

--- main.py
+++ main.py
@@ -5,6 +5,13 @@ from ai_model import analyze_data
 from endpoint_registry import update_endpoint, get_endpoint_metadata
 from load_balancer import update_traefik_config
 from monitor_logging import process_request
+from api_gateway import app
+from message_queue import send_message
+from ai_model import generate_initial_output, generate_diff_output
+from endpoint_registry import update_endpoint, get_endpoint_metadata
+from load_balancer import update_traefik_config
+from monitor_logging import process_request
+from diff_generator import generate_diff, diff_to_json, apply_diff
 import asyncio
 
 app = FastAPI()
